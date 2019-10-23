5bbac4eb7aa84e6f132699b41589a6c1f527d49c
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2019-10-23 21:25:13 +0200

    Show gitinfo as webpage

--- src/main/java/nl/bsoft/mybatch/controller/JobsController.java
+++ src/main/java/nl/bsoft/mybatch/controller/JobsController.java
@@ -11,6 +11,7 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +19,8 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -48,37 +51,19 @@ public class JobsController {
     }
 
     @GetMapping("/gitinfo")
-    public String gitinfo() throws Exception {
+    public String gitinfo(Model model) throws Exception {
         logger.debug("Start gitinfo");
 
         GitProperties gitProperties = new GitProperties();
 
-        StringBuffer result = new StringBuffer();
-        result.append(GitProperties.BRANCH + ": " + gitProperties.getBranch());
-        result.append("\n" + GitProperties.BUILD_TIME + ": " + gitProperties.getBuildTime());
-        result.append("\n" + GitProperties.BUILD_USER_EMAIL + ": " + gitProperties.getBuildUserEmail());
-        result.append("\n" + GitProperties.BUILD_USER_NAME + ": " + gitProperties.getBuildUserName());
-        result.append("\n" + GitProperties.BUILD_VERSION + ": " + gitProperties.getBuildVersion());
-        result.append("\n" + GitProperties.CLOSEST_TAG_COMMIT_COUNT + ": " + gitProperties.getClosestTagCommitCount());
-        result.append("\n" + GitProperties.CLOSEST_TAG_NAME + ": " + gitProperties.getClosestTagName());
-        result.append("\n" + GitProperties.COMMIT_ID + ": " + gitProperties.getCommitId());
-        result.append("\n" + GitProperties.COMMIT_ID_ABBREV + ": " + gitProperties.getCommitIdAbbrev());
-        result.append("\n" + GitProperties.COMMIT_ID_DESCRIBE + ": " + gitProperties.getCommitIdDescribe());
-        result.append("\n" + GitProperties.COMMIT_ID_DESCRIBE_SHORT + ": " + gitProperties.getCommitIdDescribeShort());
-        result.append("\n" + GitProperties.COMMIT_MESSAGE_FULL + ": " + gitProperties.getCommitMessageFull());
-        result.append("\n" + GitProperties.COMMIT_MESSAGE_SHORT + ": " + gitProperties.getCommitMessageShort());
-        result.append("\n" + GitProperties.COMMIT_TIME + ": " + gitProperties.getCommitTime());
-        result.append("\n" + GitProperties.COMMIT_USER_EMAIL + ": " + gitProperties.getCommitUserEmail());
-        result.append("\n" + GitProperties.COMMIT_USER_NAME + ": " + gitProperties.getCommitUserName());
-        result.append("\n" + GitProperties.DIRTY + ": " + gitProperties.getDirty());
-        result.append("\n" + GitProperties.HOST + ": " + gitProperties.getHost());
-        result.append("\n" + GitProperties.LOCAL_BRANCH_AHEAD + ": " + gitProperties.getLocalBranchAhead());
-        result.append("\n" + GitProperties.LOCAL_BRANCH_BEHIND + ": " + gitProperties.getLocalBranchBehind());
-        result.append("\n" + GitProperties.REMOTE_ORIGIN_URL + ": " + gitProperties.getRemoteOriginUrl());
-        result.append("\n" + GitProperties.TAGS + ": " + gitProperties.getTags());
-        result.append("\n" + GitProperties.TOTAL_COMMIT_COUNT + ": " + gitProperties.getTotalCommitCount());
+        List<String> gitLines = new ArrayList<String>();
 
-        return result.toString();
+        gitProperties.GIT_PROPERTIES.forEach((propertyName) -> {
+            gitLines.add(propertyName + ": " + gitProperties.getPropertie(propertyName));
+        });
+
+        model.addAttribute("gitLines", gitLines);
+        return "/gitinfodetail";
     }
 
     @GetMapping("/csvtopostgreslimit")
@@ -92,7 +77,7 @@ public class JobsController {
     @GetMapping("/postgrestoh2")
     public String postgrestoh2() throws Exception {
 
-        String result = "ready";
+        String result = "/ready";
 
         JobParameters parameters = new JobParametersBuilder()
                 .addString("key", "empty")
@@ -101,7 +86,7 @@ public class JobsController {
         try {
             jobLauncher.run(postgres2H2Job, parameters);
         } catch (Exception e) {
-            result = "error";
+            result = "/error";
         }
         return result;
     }
@@ -149,7 +134,7 @@ public class JobsController {
             throw new Exception("fileName parameter expected but not present");
         }
 
-        String result = "ready";
+        String result = "/ready";
         long now = System.currentTimeMillis();
         LocalDateTime date = LocalDateTime.ofInstant(Instant.ofEpochMilli(now), ZoneId.systemDefault());
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss.SSS");
@@ -164,7 +149,8 @@ public class JobsController {
         try {
             jobLauncher.run(fileToPostgresSkipJob, parameters);
         } catch (Exception e) {
-            result = "error";
+            log.error("Error in job fileToPostgresSkipJob");
+            result = "/error";
         }
         return result;
     }
